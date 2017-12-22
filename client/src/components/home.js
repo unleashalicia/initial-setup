a996631e9cb5e418833d9141e70fc983a482e313
unleashalicia <alicia.adams.evans@gmail.com>
2017-12-22 12:35:53 -0800

    Finshed react setup

---
+++ client/src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default props => {
+    return (
+        <div>
+            <h1>
+                This is the home
+            </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea est eum expedita fugit minus nulla officia officiis, praesentium quas qui repudiandae sunt, veritatis vero? Dolores fugiat, voluptate. Enim fugiat, porro?</p>
+        </div>
+    );
+}
